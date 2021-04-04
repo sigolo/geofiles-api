f51c248e08995e599c912833ce25bb4b769630f3
jossefaz <jossefaz@protonmail.com>
2021-04-04 14:48:29 +0300

    init project with OSGEO + LIbreDWG Dockerfile

--- src/app/api/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Item(BaseModel):
-    name: str = Field(...)
-
-class PublicItem(BaseModel):
-    id:int
--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
-from .api import monitor, items
-from .db.db_engine import engine, database
-from .db.db_models import metadata
-
-metadata.create_all(engine)
-
+from .api import monitor
 
 app = FastAPI()
 
@@ -19,7 +13,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -28,15 +21,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
 app.include_router(monitor.router, prefix="/monitor", tags=["monitoring"])
-app.include_router(items.router, prefix="/items", tags=["items"])
