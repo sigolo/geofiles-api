8329ee0be9c740c506a37388d2705caeef9d0cbe
jossefaz <jossefaz@protonmail.com>
2021-04-13 00:51:24 +0300

    format log

--- src/app/utils/logs.py
+++ src/app/utils/logs.py
@@ -1,5 +1,4 @@
 
-
 def log_http_response(request_id, formatted_process_time, status_code):
-    log = {"request_id": request_id, "completed_in": formatted_process_time, "status_code": status_code}
+    log = {"request_id": request_id, "completed_in": f"{formatted_process_time}ms", "status_code": status_code}
     return log
