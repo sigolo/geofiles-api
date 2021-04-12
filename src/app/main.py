aab508d8d8b0501bf5da67616c8cc711049a0779
jossefaz <jossefaz@protonmail.com>
2021-04-13 00:49:45 +0300

    add request logger middleware

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,21 @@
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+import json
+import uuid
 
+from .utils.logs import log_http_response
+import sys
+import time
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
 from .api import monitor, files
 from .db.db_engine import engine, database
 from .db.db_models import metadata
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stdout, colorize=True, format="{message}")
+
 
 metadata.create_all(engine)
-
 
 app = FastAPI()
 
@@ -26,6 +35,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def add_request_id_process_time_header(request: Request, call_next):
+    request_id = str(uuid.uuid4()) if "X-Request-ID" not in request.headers else request.headers["X-Request-ID"]
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = '{0:.2f}'.format(process_time)
+    response.headers["X-Process-Time"] = str(process_time)
+    response.headers["X-Request-ID"] = request_id
+    log = log_http_response(request_id,formatted_process_time,response.status_code)
+    logger.info(json.dumps(log))
+    return response
 
 
 @app.on_event("startup")
@@ -36,6 +57,7 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
 
 
 app.include_router(monitor.router, prefix="/monitor", tags=["monitoring"])
---
+++ src/app/utils/logs.py
@@ -0,0 +1,5 @@
+
+
+def log_http_response(request_id, formatted_process_time, status_code):
+    log = {"request_id": request_id, "completed_in": formatted_process_time, "status_code": status_code}
+    return log
