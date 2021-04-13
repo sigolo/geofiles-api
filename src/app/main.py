111ecab7718bf7675077ef8d1b92ce6cc3a60761
jossefaz <jossefaz@protonmail.com>
2021-04-13 18:40:58 +0300

    update logger to signlton instance

--- src/app/main.py
+++ src/app/main.py
@@ -1,24 +1,23 @@
-import json
 import uuid
-
-from .utils.logs import log_http_response
-import sys
 import time
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from .api import monitor, files
 from .db.db_engine import engine, database
 from .db.db_models import metadata
-from loguru import logger
+from .utils.logs import RestLogger
 
-logger.remove()
-logger.add(sys.stdout, colorize=True, format="{message}")
-
-
+LogInstance = RestLogger.instance
+RestLogger.init_logger()
 metadata.create_all(engine)
-
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -27,13 +26,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.middleware("http")
 async def add_request_id_process_time_header(request: Request, call_next):
@@ -44,8 +36,8 @@ async def add_request_id_process_time_header(request: Request, call_next):
     formatted_process_time = '{0:.2f}'.format(process_time)
     response.headers["X-Process-Time"] = str(process_time)
     response.headers["X-Request-ID"] = request_id
-    log = log_http_response(request_id,formatted_process_time,response.status_code)
-    logger.info(json.dumps(log))
+    LogInstance.request_id = request_id
+    LogInstance.log_http_response(formatted_process_time, response.status_code)
     return response
 
 
@@ -57,7 +49,6 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
 
 
 app.include_router(monitor.router, prefix="/monitor", tags=["monitoring"])
--- src/app/utils/logs.py
+++ src/app/utils/logs.py
@@ -1,4 +1,60 @@
+import json
+import sys
+import threading
+import uuid
+from loguru import logger
+from enum import Enum
 
-def log_http_response(request_id, formatted_process_time, status_code):
-    log = {"request_id": request_id, "completed_in_ms": formatted_process_time, "status_code": status_code}
-    return log
+
+class LogType(str,Enum):
+    HTTP = "HTTP"
+    SQL = "SQL"
+
+
+class SingletonMeta(type):
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cls._instance = None
+        cls._locker = threading.Lock()
+
+    @property
+    def instance(self, *args, **kwargs):
+        if self._instance is None:
+            with self._locker:
+                if self._instance is None:
+                    self._instance = self(*args, **kwargs)
+        return self._instance
+
+
+class RestLogger(metaclass=SingletonMeta):
+    def __init__(self):
+        self._request_id = None
+
+    @classmethod
+    def init_logger(cls):
+        logger.remove()
+        logger.add(sys.stdout, colorize=True, format="{message}")
+
+    @property
+    def request_id(self):
+        if not self._request_id:
+            return None
+        return str(self._request_id)
+
+    @request_id.setter
+    def request_id(self, request_id):
+        try:
+            self._request_id = uuid.UUID(str(request_id))
+        except ValueError:
+            raise ValueError("request id must be a UUID string")
+
+    def log_http_response(self, formatted_process_time, status_code):
+        log = {"request_id": self.request_id, "log_type": LogType.HTTP, "completed_in_ms": formatted_process_time,
+               "status_code": status_code}
+        logger.info(json.dumps(log))
+
+    def log_sql_query(self, sql_query, record_num):
+        log = {"request_id": self.request_id, "log_type": LogType.SQL, "query": sql_query}
+        if record_num:
+            log["record_found"] = record_num
+        logger.info(json.dumps(log))
